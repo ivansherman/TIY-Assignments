13636422485b2944b5bec669426568eb96575de6
Ivan Sherman <ivansherman@me.com>
2015-01-20 09:45:48 -0500

    adds a little change (shorter)

--- code/poker/deck_test.rb
+++ code/poker/deck_test.rb
@@ -8,16 +8,12 @@ class DeckTest < MiniTest::Unit::TestCase
 		@deck = Deck.new
 	end
 
-	def test_face_card_displays_its_suit_and_value
-		@Face_card = Card.new("jack", :spades)
-		assert_equal "King of Spades", @Face_card.display
-	end
 
 	def test_deck_has_52_cards
 		assert_equal 52, @deck.size
 	end
 
-	def test_deck_can_deal_and_cards_are_removed_from_deck
+	def test_deck_can_deal
 		cards = @deck.deal(3)
 		assert_equal 49, @deck.size
 	end
