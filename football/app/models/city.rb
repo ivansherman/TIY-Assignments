7523bf3373be718551caca38e023b078253057a5
Ivan Sherman <ivansherman@me.com>
2015-01-27 16:01:03 -0500

    started to define methods

--- football/app/models/city.rb
+++ football/app/models/city.rb
@@ -2,8 +2,10 @@ class City < ActiveRecord::Base
 	belongs_to :state
 
 	def fastest_growing
+		cities.order('population_change DESC').index[0][0]
 	end
 
 	def shrinking
+		order(:change)
 	end
 end
